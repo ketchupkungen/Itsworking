5df81b6ff6f93759f7fcc99efb62b0078a5f64a8
Michael Woode <info@woodesoft.com>
2017-02-05 14:21:43 +0100

    Added a form for profile page (for menu item "Inställningar?")

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,42 @@
+
 $(document).ready(function () {
-    
+	console.log('Yo! DOM Ready!');
+
+
+	//adds 'click' event listener for menu items
+	initMenuItemClick();
+
+
 });
+
+
+function initMenuItemClick() {
+
+	// click on menu item
+    $('html').on('click', '.navbar-nav li', function (e) {
+   		console.log('You selected menu item (object): ', e);
+
+   		if (!e.target.id){
+	   		console.log('The menu item "'+e.target.text+'" is MISSING an id!!');
+   		} else{
+	   		console.log('You selected menu item (id): ', e.target.id);
+   		}
+
+   		var whichTemplate = '';
+
+   		if (e.target.id == 'profile-settings') {
+   			whichTemplate = 'profileContent';
+   		}
+
+   		//!!TODO: add more menu items here
+
+
+   		if (whichTemplate !== '') {
+			var htmlTemplate = loadTemplate('layouts/'+whichTemplate+'.html');
+			$('.header-title h1').html(e.target.text);
+			$('main').html(htmlTemplate);
+		}
+	});
+
+}
+
